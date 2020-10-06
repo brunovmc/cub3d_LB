d5f43c454a8495348b49fd9675432b12027042b9
Laercio Candido <lcs_junior@yahoo.com.br>
2020-10-06 17:52:33 +0200

    testes de passar ponteiro por varias funçoes

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,28 +1,51 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "mlx.h"
+# include "../minilibx-linux/mlx.h"
 # include <math.h>
 # include <stdio.h>
+# include <stdlib.h>
 
-#define TILE_SIZE 40
-#define MAP_NUM_ROWS 10
-#define MAP_NUM_COLS 15
-#define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
-#define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
+# define TILE_SIZE 40
+# define MAP_NUM_ROWS 10
+# define MAP_NUM_COLS 15
+# define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
+# define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
 
-typedef struct s_data {
+# define ESC 0x00ff1b
+# define W_KEY 0x77
+# define A_KEY 0x61
+# define S_KEY 0x73
+# define D_KEY 0x64
+
+# define POSX 100
+# define POSY 100
+
+typedef struct s_point {
+    int       x;
+    int       y;
+    int       color;
+}               t_point;
+
+typedef struct s_player {
+    t_point     *pos;
+}               t_player;
+
+typedef struct  s_data {
 	void	*img;
 	char	*addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-}		t_data;
+	int	    bits_per_pixel;
+	int	    line_length;
+	int	    endian;
+}		        t_data;
 
-typedef struct s_vars {
-	void	*mlx;
-	void	*window;
-}		t_vars;
+typedef struct  s_vars {
+	void	    *mlx;
+	void	    *window;
+	t_data  	*data;
+    t_player    *player;
+    t_point     *pos;
+}               t_vars;
 
 const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -39,11 +62,12 @@ const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
 
 void    my_pixel_put(t_data *data, int x, int y, int color);
 void    put_rect(t_data *data, int x, int y, int size_x, int size_y, int color);
-void    renderMap(t_vars vars, t_data data);
+void    renderMap();
 void    put_circle(t_data *data, int a, int b, int size, int color);
 void    print_str(t_vars *vars,  int x, int y, int color, char *str);
 int     close(t_vars *vars);
 int     keypressed(int key, t_vars *vars);
+t_data	*create(void *mlx);
 
 
 
--- srcs/main.c
+++ srcs/main.c
@@ -1,38 +1,10 @@
-# include "mlx.h"
-# include <math.h>
-# include <stdio.h>
+#include "cub3d.h"
 
-#define TILE_SIZE 60
-#define MAP_NUM_ROWS 10
-#define MAP_NUM_COLS 15
-#define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
-#define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
-
-typedef struct s_data {
-	void	*img;
-	char	*addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-}		t_data;
-
-typedef struct s_vars {
-	void	*mlx;
-	void	*window;
-}		t_vars;
-
-const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1},
-    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-};
+int	close(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->window);
+	exit(0);
+}
 
 void	my_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -42,31 +14,15 @@ void	my_pixel_put(t_data *data, int x, int y, int color)
        *(unsigned int*)dst = color;       
 }
 
-void	put_rect(t_data *data, int x, int y, int size_x, int size_y, int color)
-{
-	int init_x;
-
-	init_x = x;
-	while (y <= size_y)
-	{
-		x = init_x;
-		while (x <= size_x)
-		{
-			my_pixel_put(data, x, y, color);
-			x++;
-		}
-		y++;
-	}
-}
-
-void    renderMap(t_vars *vars, t_data *data)
+void    renderMap()
 {
     int i;
     int j;
     int tileX;
     int tileY;
     int tileColor;
-
+	t_data	data;
+    
     i = 0;
     while (i < MAP_NUM_ROWS)
     {
@@ -76,18 +32,29 @@ void    renderMap(t_vars *vars, t_data *data)
             tileX = j * TILE_SIZE;
             tileY = i * TILE_SIZE;
             tileColor = map[i][j] != 1 ? 0X00FFFFFF : 0X00000000;
-            //render_rectangle(vars, tileX, tileY, tileX + TILE_SIZE, tileY + TILE_SIZE, tileColor);
-			put_rect(data, tileX, tileY, tileX + TILE_SIZE, tileY + TILE_SIZE, tileColor);
+			put_rect(&data, tileX, tileY, tileX + TILE_SIZE, tileY + TILE_SIZE, tileColor);
             j++;
         }
         i++;
     }
 }
 
-int	close(t_vars *vars)
+void	put_rect(t_data *data, int x, int y, int size_x, int size_y, int color)
 {
-	mlx_destroy_window(vars->mlx, vars->window);
-	exit(0);
+	int a;
+	int b;
+
+	a = 0;
+	while (a <= size_x)
+	{
+		b = 0;
+		while (b <= size_y)
+		{
+			my_pixel_put(data, a + x, b + y, color);
+			b++;
+		}
+		a++;
+	}
 }
 
 void	put_circle(t_data *data, int a, int b, int size, int color)
@@ -109,6 +76,32 @@ void	put_circle(t_data *data, int a, int b, int size, int color)
 		x++;
 	}
 }
+/*
+void	teste2(t_data *data, int x, int y, int color)
+{
+	put_circle(data, x, y, 5, color);
+}
+
+void	teste(t_data *data, int x, int y, int color)
+{
+	teste2(data, x, y, color);
+}
+*/
+int		update_frame(t_vars *vars)
+{
+	t_data data;
+
+	data.img = mlx_new_image(vars->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+
+	//renderMap();
+	//put_rect(&data, vars->pos->x, vars->pos->y, 15, 15, 0x0000ff00);
+	put_circle(&data, vars->pos->x, vars->pos->y, 5, 0x00ff0000);
+	//teste(&data, vars->pos->x + POSX, vars->pos->y + POSY, 0x0000ff00);
+	
+	mlx_put_image_to_window(vars->mlx, vars->window, data.img, 0, 0);
+	return 0;
+}
 
 int		keypressed(int key, t_vars *vars)
 {
@@ -117,83 +110,49 @@ int		keypressed(int key, t_vars *vars)
 	static int		x;
 	static int		y;
 
-	x = 100;
-	y = 100;
-	data.img = mlx_new_image(vars->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-
-	if (key == 113)
-		return(close(&vars));
-	else if (key == 122)
-		//put_circle(data.img, 249, 249, 249, 0x00ff0000);
-		//mlx_string_put(vars->mlx, vars->window, 100, 100, 0x00ff0000, "Funciona, carai");
-		y += 1;
-
-	else if (key == 120)
-//		put_circle(data.img, 249, 249, 249, 0x0000ff00);
-	//mlx_string_put(vars->mlx, vars->window, 200, 100, 0x0000ff00, "Funciona, carai");
-		x -= 1;
-
-	else if (key == 99)
-		//mlx_string_put(vars->mlx, vars->window, 300, 100, 0x000000ff, "Funciona, carai");
-//		put_circle(data.img, 249, 249, 249, 0x000000ff);
-		y -= 1;
-	else if (key == 119)
-	//mlx_string_put(vars->mlx, vars->window, 400, 100, 0x0000ffff, "Funciona, carai");
-//	put_circle(data.img, 249, 249, 249, color);
-		x += 1;
-
-	mlx_string_put(vars->mlx, vars->window, x, y, 0x0000ffff, "Funciona, carai");
-
-	//mlx_put_image_to_window(vars->mlx, vars->window, data.img, 0, 0);
-	//mlx_loop(vars->mlx);
-
-	//return(key);
-}
-
-
-int		update_frame(t_vars *vars)
-{
-	int		i;
-	int		color;
-	t_data	data;
-
-	data.img = mlx_new_image(vars->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-
-	//	i = keypressed(key, vars);
-	//mlx_hook(vars.window, 2, 1L<<0, keypressed, &vars);
-	{ 
-		if (i % 3 == 0)
-			color = 0x00ff0000;
-		else if (i % 3 == 1)
-			color = 0x0000ff00;
-		else
-			color = 0x000000ff;
-		put_circle(data.img, 249, 249, 249, color);
+	if (key == W_KEY)
+	{
+		//y -= 10;
+		vars->pos->y -= 10;
+		//printf("%d\n", vars->player->pos->y);
 	}
-	mlx_put_image_to_window(vars->mlx, vars->window, data.img, 0, 0);
-	mlx_loop(vars->mlx);
+	else if (key == A_KEY)
+	{
+		//x -= 10;
+		vars->pos->x -= 10;
+		//printf("%d\n", vars->player->pos->x);
+	}
+	else if (key == S_KEY)
+	{
+		//y += 10;
+		vars->pos->y += 10;
+		//printf("%d\n", vars->player->pos->y);
+	}
+	else if (key == D_KEY)
+	{
+		//x += 10;
+		vars->pos->x += 10;
+		//printf("%d\n", vars->player->pos->x);
+	}
+	else if (key == ESC)
+		return (close(vars));
 
+	//vars->pos->x = x;
+	//vars->pos->y = y;
+	update_frame(vars);
+
+	return(key);
 }
-
 
 int	main(void)
 {
 	t_vars	vars;
-	t_data	data;
 
 	vars.mlx = mlx_init();
 	vars.window = mlx_new_window(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "TESTE PRIMEIRO");
-	data.img = mlx_new_image(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-
-	//put_circle(&data, 249, 249, 249, 0x0000FF00);
-	//renderMap(&vars, &img);
-	//mlx_put_image_to_window(vars.mlx, vars.window, img.img, 0, 0);	
+	renderMap(); ////////////////////////
 	mlx_hook(vars.window, 2, 1L<<0, keypressed, &vars);
 	//mlx_loop_hook(vars.mlx, update_frame, &vars);
-	//sethooks(&vars);
 	mlx_loop(vars.mlx);
 	return (0);
 }
