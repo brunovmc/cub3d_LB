d173a325ffe5c0cbcb6fb850e1baf3a3cf81f537
brunovmc <bruno.vidigal.coriolano@gmail.com>
2020-10-13 00:43:52 +0200

    mapa e jogador

--- srcs/main.c
+++ srcs/main.c
@@ -1,119 +1,45 @@
 #include "cub3d.h"
 
-//const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
-  //  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    //{1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    //{1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    //{1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    //{1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1},
-    //{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-    //{1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    //{1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1},
-    //{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    //{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-//};
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	char	*mem;
-	size_t	i;
-	size_t	len;
-
-	len = count * size;
-	mem = malloc(len);
-	if (mem == 0)
-		return (0);
-	i = 0;
-	while (len > 0)
-	{
-		mem[i] = '\0';
-		i++;
-		len--;
-	}
-	return (mem);
-}
-
-void	my_pixel_put(t_data *data, int x, int y, int color)
-{
-	char 	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-       *(unsigned int*)dst = color;       
-}
-
-void	put_rect(t_data *data, int x, int y, int size_x, int size_y, int color)
-{
-	int a;
-	int b;
-
-	a = 0;
-	while (a <= size_x)
-	{
-		b = 0;
-		while (b <= size_y)
-		{
-			my_pixel_put(data, a + x, b + y, color);
-			b++;
-		}
-		a++;
-	}
-}
-
-void    renderMap(t_data data)
-{
-    int i;
-    int j;
-    int tileX;
-    int tileY;
-    int tileColor;
-
-	//data = alloc_data(vars);
-    
-    i = 0;
-    while (i < MAP_NUM_ROWS)
-    {
-        j = 0;
-        while (j < MAP_NUM_COLS)
-        {
-            tileX = j * TILE_SIZE;
-            tileY = i * TILE_SIZE;
-            tileColor = map[i][j] != 1 ? 0X00FFFFFF : 0X00000000;
-            //render_rectangle(vars, tileX, tileY, tileX + TILE_SIZE, tileY + TILE_SIZE, tileColor);
-			put_rect(&data, tileX, tileY, tileX + TILE_SIZE, tileY + TILE_SIZE, tileColor);
-            j++;
-        }
-        i++;
-    }
-	//mlx_put_image_to_window(vars->mlx, vars->window, data.img, 0, 0);
-}
-
-int	close(t_vars *vars)
+int close(t_vars *vars)
 {
 	mlx_destroy_window(vars->mlx, vars->window);
 	exit(0);
 }
 
-void	put_circle(t_data *data, int a, int b, int size, int color)
+/*
+void	sethooks(t_vars *vars)
 {
-	int x;
-	int y;
+	mlx_hook(vars->window, 2, 0, keypressed, &vars);
+}
+*/
 
-	x = 0;
-	while (x <= (a + size))
+// porque essa funcao funciona???
+void my_pixel_put(t_data *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void put_rectangle(t_data *data, int x, int y, int size_x, int size_y, int color)
+{
+	int init_x;
+
+	init_x = x;
+	while (y <= size_y)
 	{
-		y = 0;
-		while (y <= (b + size) )
+		x = init_x;
+		while (x <= size_x)
 		{
-			if ((x - a) * (x - a) + (y - b) * (y - b) <= size * size)
-				my_pixel_put(data, x, y, color);
-			y++;
-
+			my_pixel_put(data, x, y, color);
+			x++;
 		}
-		x++;
+		y++;
 	}
 }
 
-void	put_line(t_data *data, t_vars *vars, double x, double y, int size, int color)
+void put_line(t_data *data, t_vars *vars, double x, double y, int size, int color)
 {
 	double dir;
 	int i;
@@ -122,7 +48,7 @@ void	put_line(t_data *data, t_vars *vars, double x, double y, int size, int colo
 
 	i = 0;
 	//i = vars->player->walk_forward;
-	dir = (vars->player->dir * 3.14)/180;
+	dir = (vars->player->dir * 3.14) / 180;
 	while (i < size)
 	{
 		vars->pos->new_x = (size - i) * cos(dir);
@@ -138,28 +64,52 @@ void	put_line(t_data *data, t_vars *vars, double x, double y, int size, int colo
 
 		i++;
 	}
-	
 }
 
-//void	teste(t_vars *vars, int x, int y, int color)
-int		update_frame(t_vars *vars)
+void put_circle(t_data *data, int a, int b, int size, int color)
 {
-	t_data data;
+	int x;
+	int y;
 
-	// data.img = mlx_new_image(vars->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	// data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-	data = alloc_data(vars);
-	//renderMap(data);
-	//renderMap(&vars , &data);
-	//my_pixel_put(&data, POSX + x, POSY + y, color);
-	//put_rect(&data, vars->pos->x, vars->pos->y, 50, 1, 0x00ff0000);
-	put_circle(&data, vars->pos->x, vars->pos->y, 10, 0x00ff0000);
-	put_line(&data, vars, vars->pos->x, vars->pos->y, 100, 0x0ff0000);
-	mlx_put_image_to_window(vars->mlx, vars->window, data.img, 0, 0);
-	return (0);
+	x = 0;
+	while (x <= (a + size))
+	{
+		y = 0;
+		while (y <= (b + size))
+		{
+			if ((x - a) * (x - a) + (y - b) * (y - b) <= size * size)
+				my_pixel_put(data, x, y, color);
+			y++;
+		}
+		x++;
+	}
 }
 
-void	walk_forward(t_vars *vars)
+void renderMap(t_data data)
+{
+	int i;
+	int j;
+	int tileX;
+	int tileY;
+	int tileColor;
+
+	i = 0;
+	while (i < MAP_NUM_ROWS)
+	{
+		j = 0;
+		while (j < MAP_NUM_COLS)
+		{
+			tileX = j * TILE_SIZE;
+			tileY = i * TILE_SIZE;
+			tileColor = map[i][j] != 1 ? 0X00FFFFFF : 0X00000000;
+			put_rectangle(&data, tileX, tileY, tileX + TILE_SIZE, tileY + TILE_SIZE, tileColor);
+			j++;
+		}
+		i++;
+	}
+}
+
+void walk_forward(t_vars *vars)
 {
 	//vars->pos->x += 10;
 	//vars->player->walk_forward += 1;
@@ -189,9 +139,9 @@ void rotation_sign(t_vars *vars)
 {
 	if (vars->player->dir > 360 - ROTATION_SPEED)
 		vars->player->dir = 0;
-	if (vars->player->dir == - ROTATION_SPEED)
+	if (vars->player->dir == -ROTATION_SPEED)
 		vars->player->dir = 360 - ROTATION_SPEED;
-}	
+}
 
 void look_left(t_vars *vars)
 {
@@ -209,9 +159,9 @@ void look_right(t_vars *vars)
 	printf("vars->player->dir %f\n", vars->player->dir);
 }
 
-int		keypressed(int key, t_vars *vars)
+int keypressed(int key, t_vars *vars)
 {
-	int		color;
+	int color;
 
 	if (key == W_KEY)
 		walk_forward(vars);
@@ -226,30 +176,33 @@ int		keypressed(int key, t_vars *vars)
 	else if (key == RIGHT_KEY)
 		look_right(vars);
 	else if (key == ESC)
-	 	return (close(vars));
+		return (close(vars));
 
 	update_frame(vars);
-	return(key);
+	return (key);
 }
 
-t_data 	alloc_data(t_vars *vars)
+int update_frame(t_vars *vars)
 {
-	t_data *data;
+	t_data data;
 
-	data = ft_calloc(1, sizeof(t_data));
-
-	data->img = mlx_new_image(vars->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length, &data->endian);
-
-	return(*data);
+	data.img = mlx_new_image(vars->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	//data = alloc_data(vars);
+	renderMap(data);
+	//renderMap(&vars , &data);
+	//my_pixel_put(&data, POSX + x, POSY + y, color);
+	//put_rect(&data, vars->pos->x, vars->pos->y, 50, 1, 0x00ff0000);
+	put_circle(&data, vars->pos->x, vars->pos->y, 10, 0x00ff0000);
+	put_line(&data, vars, vars->pos->x, vars->pos->y, 100, 0x0ff0000);
+	mlx_put_image_to_window(vars->mlx, vars->window, data.img, 0, 0);
+	return (0);
 }
 
-
-
-int	main(void)
+int main(void)
 {
-	t_vars	vars;
-	t_point pos;	
+	t_vars vars;
+	t_point pos;
 	t_player player;
 
 	player.dir = 0.0;
@@ -258,23 +211,12 @@ int	main(void)
 	pos.y = POSY;
 	vars.pos = &pos;
 
-	
-
 	vars.mlx = mlx_init();
 	vars.window = mlx_new_window(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "TESTE PRIMEIRO");
 
-	// printf("%i\n", vars.pos->x);
-	// printf("%i\n", pos.x);
-
-	// printf("%f\n", vars.player->dir);
-	// printf("%f\n", player.dir);
-	// printf("%f\n", pos.x);
-	// printf("%f\n", pos.y);
-
 	update_frame(&vars);
-
 	mlx_hook(vars.window, 2, 1L << 0, keypressed, &vars);
-	//mlx_loop_hook(vars.mlx, update_frame, &vars);
+
 	mlx_loop(vars.mlx);
 	return (0);
 }
